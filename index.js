28819feb784afbb4d544fcb294f0b1042d2bb81b
Nitin Chaudhary <nitin.io@outlook.com>
2024-09-18 14:23:45 +0530

    added clearStr function to make folder names simpler

--- index.js
+++ index.js
@@ -14,30 +14,49 @@ const config = {
 };
 
 const directories = [config.inputFolderPath];
+const clearStr = (str) =>
+  str
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-\/\\\\.]+/g, "")
+    .replace(/-$/, "")
+    .replace(/[-_]+/g, "_") // Replace -_ with _
+    .replace(/[-_]\.+/g, ".")
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
 
 (async () => {
   while (directories.length > 0) {
     const directory = directories.shift();
+    const toCreateFolder = clearStr(
+      path.join(directory.replace("assets", "compressed"))
+    );
     stats.directories++;
+    fs.mkdirSync(toCreateFolder, {
+      recursive: true,
+    });
     const data = fs.readdirSync(directory, { withFileTypes: true });
     directories.push(
       ...data
         .filter((file) => file.isDirectory())
         .map((file) => path.join(directory, file.name))
     );
-    const files = data
-      .filter((file) => !file.isDirectory());
+    const files = data.filter((file) => !file.isDirectory());
     // const file = path.join(directory, file.name);
     files.forEach(async (file) => {
-      console.log(file.name, stats.files);
       stats.files++;
       const buffer = fs.readFileSync(path.join(file.path, file.name));
       const compressed = await sharp(buffer)
-        .resize(config.width, config.height)
-        .toBuffer();
-      fs.mkdirSync(path.join(file.path.replace("assets", "compressed")), { recursive: true });
+      .resize(config.width, config.height)
+      .toBuffer();
+      console.log(clearStr(file.name));
       fs.writeFileSync(
-        path.join(file.path.replace("assets", "compressed"), file.name),
+        path.join(
+          clearStr(file.path.replace("assets", "compressed")),
+          clearStr(file.name)
+        ),
         compressed
       );
     });
